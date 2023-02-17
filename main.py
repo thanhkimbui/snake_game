f4dbeb9c7f820a8f25e218d839b335d49f74700f
Kim Thanh Bui <thanh.kimm.bui@gmail.com>
2023-02-17 01:07:07 -0800

    save high score working with files

--- main.py
+++ main.py
@@ -36,15 +36,13 @@ while game_is_on:
 
     # Detects collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detects collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-    # If head collides with any segment in the tail => trigger game_over
-
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", mode="r") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -17,12 +19,17 @@ class Scoreboard(Turtle):
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}   High Score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=GAME_OVER_FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score))
+
+        self.score = 0
+        self.update_scoreboard()
